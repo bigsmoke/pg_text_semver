6ee98ae7dd3e5728db1f488fc64fa4a782915e42
Rowan <rowan@bigsmoke.us>
2023-05-13 16:02:43 +0100

    Add _Authors and contributors_ section to `README.md`

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ---
 pg_extension_name: pg_text_semver
-pg_extension_version: 0.1.2
-pg_readme_generated_at: 2023-04-30 22:00:55.282082+01
-pg_readme_version: 0.6.1
+pg_extension_version: 0.1.3
+pg_readme_generated_at: 2023-05-13 16:02:17.757881+01
+pg_readme_version: 0.6.3
 ---
 
 # PostgreSQL semantic versioning extension: `pg_text_semver`
@@ -232,6 +232,22 @@ CREATE TYPE semver_parsed AS (
   build text
 );
 ```
+
+## Authors and contributors
+
+* [Rowan](https://www.bigsmoke.us/) originated this extension in 2022 while
+  developing the PostgreSQL backend for the [FlashMQ SaaS MQTT cloud
+  broker](https://www.flashmq.com/).  Rowan does not like to see himself as a
+  tech person or a tech writer, but, much to his chagrin, [he
+  _is_](https://blog.bigsmoke.us/category/technology). Some of his chagrin
+  about his disdain for the IT industry he poured into a book: [_Why
+  Programming Still Sucks_](https://www.whyprogrammingstillsucks.com/).  Much
+  more than a “tech bro”, he identifies as a garden gnome, fairy and ork rolled
+  into one, and his passion is really to [regreen and reenchant his
+  environment](https://sapienshabitat.com/).  One of his proudest achievements
+  is to be the third generation ecological gardener to grow the wild garden
+  around his beautiful [family holiday home in the forest of Norg, Drenthe,
+  the Netherlands](https://www.schuilplaats-norg.nl/) (available for rent!).
 
 ## Colophon
 
---
+++ sql/pg_text_semver--0.1.2--0.1.3.sql
@@ -0,0 +1,50 @@
+-- Complain if script is sourced in `psql`, rather than via `CREATE EXTENSION`.
+\echo Use "CREATE EXTENSION pg_text_semver" to load this file. \quit
+
+--------------------------------------------------------------------------------------------------------------
+
+comment on extension pg_text_semver is
+$markdown$
+# PostgreSQL semantic versioning extension: `pg_text_semver`
+
+[![PGXN version](https://badge.fury.io/pg/pg_text_semver.svg)](https://badge.fury.io/pg/pg_text_semver)
+
+The `pg_text_semver` extension offers a `semver` `DOMAIN` type based on
+Postgres' built-in `text` type that implements the [Semantic Versioning 2.0.0
+Specification](https://semver.org/spec/v2.0.0.html).
+
+See the [`test__pg_text_semver()`](#procedure-test__pg_text_semver) procedure
+for examples of how to use the types, operators and functions provides by this
+extension.
+
+## Alternatives to `pg_text_semver`
+
+An excellent pre-existing alternative to `pg_text_semver` is the
+[`semver`](https://github.com/theory/pg-semver) extension.  `semver` is written
+in C and therefore more efficient than `pg_text_semver`.  But, as of March 2,
+2023, [it treats the parts of the semantic version as 32-bit
+integers](https://github.com/theory/pg-semver/issues/47), while the spec
+prescribes no limitations to the size of each number.
+
+<?pg-readme-reference?>
+
+## Authors and contributors
+
+* [Rowan](https://www.bigsmoke.us/) originated this extension in 2022 while
+  developing the PostgreSQL backend for the [FlashMQ SaaS MQTT cloud
+  broker](https://www.flashmq.com/).  Rowan does not like to see himself as a
+  tech person or a tech writer, but, much to his chagrin, [he
+  _is_](https://blog.bigsmoke.us/category/technology). Some of his chagrin
+  about his disdain for the IT industry he poured into a book: [_Why
+  Programming Still Sucks_](https://www.whyprogrammingstillsucks.com/).  Much
+  more than a “tech bro”, he identifies as a garden gnome, fairy and ork rolled
+  into one, and his passion is really to [regreen and reenchant his
+  environment](https://sapienshabitat.com/).  One of his proudest achievements
+  is to be the third generation ecological gardener to grow the wild garden
+  around his beautiful [family holiday home in the forest of Norg, Drenthe,
+  the Netherlands](https://www.schuilplaats-norg.nl/) (available for rent!).
+
+<?pg-readme-colophon?>
+$markdown$;
+
+--------------------------------------------------------------------------------------------------------------
